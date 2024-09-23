46a8fdf2cc33780ceb7e8d287adc8196899e0921
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-09-23 17:49:03 +0530

    created customer.js file & link it
    created this file in JS folder

---
+++ JS/customer.js
@@ -0,0 +1,8 @@
+function del() {
+    confirm("Are you want to delete this account permenantly ?");
+}
+
+function change() {
+    confirm("Are you want to change your password ?");
+    alert("Your password is changed successfully!");
+}
