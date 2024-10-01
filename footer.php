7b5272f276657b7233be19ecaee754c27b6f2d7d
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-10-01 17:26:46 +0530

    edited contactus page

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 
 <body>
     <footer>
-        <div class="container">
+        <div class="footer-container">
             <div class="sec_aboutus">
                 <img src="Images/logo.png" alt="logo">
                 <h2>About us</h2>
