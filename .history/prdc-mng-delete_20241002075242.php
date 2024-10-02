1f83764c85a6c816cf53de2c50f4b9e51483c8fc
AuthnSapuarachchi <authn.sapuarachchi@gmail.com>
2024-10-02 08:00:51 +0530

    product mng crud

---
+++ .history/prdc-mng-delete_20241002075242.php
@@ -0,0 +1,25 @@
+<?php
+    include 'config.php';
+
+    if(isset($_GET['deleteid'])) {
+        $pid=$_GET['deleteid'];
+
+        $sql="delete from `product` where id=$pid";
+        $result=mysqli_query($con,$sql);
+    
+        if($result){
+            header('Location:product-mng.php');
+        }else{
+            die(mysqli_error($con));
+        }
+    
+    }
+    
+    if($result) {
+        header('Location:poduct-mng.php');
+    }
+    else {
+        die(mysqli_error($con));
+    }
+
+?>
---
+++ .history/prdc-mng-display_20241001211353.php
@@ -0,0 +1,63 @@
+<?php
+include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/prdc-mng-displa.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="prdc-display">
+        <table id="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Product ID</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Discription</th>
+                    <th>Stock Quentity</th>
+                    <th>Action</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * From `product`;";
+                $result = mysqli_query($con, $sql);
+                if ($result) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $no = $row['Item_No'];
+                        $pid = $row['Product_Id'];
+                        $pname = $row['Product_name'];
+                        $pprice = $row['Price'];
+                        $pdescription = $row['Product_description'];
+                        $stock = $row['Stock_quantity'];
+
+                        echo '
+                            <tr>
+                            <th scope="row">'. $no . '</th>
+                            <td>' . $pid . '</td>
+                            <td>' . $pname . '</td>
+                            <td>' . $pqty . '</td>
+                            <td>' . $pprice . '</td>
+                            <td>
+                                <a href="prdc-mng-update.php?updateid=' . $pid . '" style="background-color: blue color: black; border: 1px solid black;">Update</a>
+                                <a href="prdc-mng-delete.php?deleteid=' . $pid . '">Delete</a>"
+                            </td>
+                            </tr>';
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
