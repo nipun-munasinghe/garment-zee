c22aeff3eb5420646087d310358e617e778ee5ff
AuthnSapuarachchi <authn.sapuarachchi@gmail.com>
2024-10-02 19:09:38 +0530

    crud inventory

---
+++ .history/inven-mng-display_20241002175355.php
@@ -0,0 +1,13 @@
+<?php
+include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/prdc-mng-displa.css">
+    <title>Document</title>
+</head>
---
+++ .history/inven-mng-display_20241002175414.php
@@ -0,0 +1,29 @@
+<?php
+include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/prdc-mng-displa.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="prdc-display">
+        <table id="table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Product ID</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Discription</th>
+                    <th>Stock Quentity</th>
+                    <th>Action</th>
+
+                </tr>
+            </thead>
